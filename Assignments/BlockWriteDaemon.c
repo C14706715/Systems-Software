bc89bf6d95362db86ad175c56e69120d2485b561
Jake Young <C14706715@mydit.ie>
2018-03-05 16:21:07 +0000

    Daemon added -> block users from editing the webpage during backup/upload phase

---
+++ Assignments/BlockWriteDaemon.c
@@ -0,0 +1,124 @@
+/*
+    C Program to block write access
+    of offline website
+    
+    Jake Young 
+    C14706715
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <syslog.h>
+#include <time.h>
+time_t now;
+struct tm *right_now;
+
+static void skeletonDaemon()
+{
+    pid_t pid;
+
+    /* Fork off the parent process */
+    pid = fork();
+
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
+    /* On success: The child process becomes session leader */
+    if (setsid() < 0)
+        exit(EXIT_FAILURE);
+
+    /* Catch, ignore and handle signals */
+    //TODO: Implement a working signal handler */
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+
+    /* Fork off for the second time*/
+    pid = fork();
+
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
+    /* Set new file permissions */
+    umask(0);
+
+    /* Change the working directory to the root directory */
+    /* or another appropriated directory */
+    chdir("/");
+
+    /* Close all open file descriptors */
+    int x;
+    for (x = sysconf(_SC_OPEN_MAX); x>=0; x--)
+    {
+        close (x);
+    }
+
+    /* Open the log file */
+    openlog ("Permission Daemon", LOG_PID, LOG_DAEMON);
+}
+
+int main()
+{
+    int Eleven_Hour, Fifty_Minutes, Zero_Hour, Ten_Minutes;
+    
+    Eleven_Hour = 23;
+    Fifty_Minutes = 50;
+    Zero_Hour = 00;
+    Ten_Minutes = 10;
+    
+    char Write_Mode[] = "0777";
+    char Read_Mode[] = "000";
+    char buf[300] = "/Users/Jake/Library/Mobile\ Documents/com\~apple\~CloudDocs/DT282/Year\ 4/Modules/Semester\ 2/Systems\ Software/Assignments/Offline/var/www/html.txt";
+    int i;
+    skeletonDaemon();
+
+    while (1)
+    {
+        //TODO: Insert daemon code here.
+        syslog (LOG_NOTICE, "Permission daemon started->Read Only");
+        
+            time(&now);
+            right_now=localtime(&now);
+
+            if(Eleven_Hour == right_now->tm_hour & Fifty_Minutes == right_now->tm_min){
+                printf("Folder Changed to Read Only Access");
+                i = strtol(Read_Mode, 0, 8);
+                if (chmod (buf,i) < 0)
+                {
+                    exit(1);
+                }
+                break;
+            }
+            else if(Zero_Hour == right_now->tm_hour & Ten_Minutes == right_now->tm_min){
+                printf("Folder Changed to Write access");
+                i = strtol(Write_Mode, 0, 8);
+                if (chmod (buf,i) < 0)
+                {
+                    exit(1);
+                }
+                break;
+            }
+        
+        sleep (20);
+       
+    }
+
+    syslog (LOG_NOTICE, "Permission daemon terminated.");
+    closelog();
+
+    return EXIT_SUCCESS;
+}
--- Assignments/move.c
+++ Assignments/move.c
@@ -4,29 +4,27 @@
 int main()
 {
     FILE *fptr1, *fptr2;
-    char filename1[300], filename2[300], c;
+    char OfflineFile[300], LiveFile[300], c;
     
-    strcpy(filename1, "/Users/Jake/Library/Mobile\ Documents/com\~apple\~CloudDocs/DT282/Year\ 4/Modules/Semester\ 2/Systems\ Software/Assignments/Offline/Offline.c");
+    strcpy(OfflineFile, "/Users/Jake/Library/Mobile\ Documents/com\~apple\~CloudDocs/DT282/Year\ 4/Modules/Semester\ 2/Systems\ Software/Assignments/Offline/Offline.c");
     
-    strcpy(filename2, "/Users/Jake/Library/Mobile\ Documents/com\~apple\~CloudDocs/DT282/Year\ 4/Modules/Semester\ 2/Systems\ Software/Assignments/Live/LiveFile.c");
+    strcpy(LiveFile, "/Users/Jake/Library/Mobile\ Documents/com\~apple\~CloudDocs/DT282/Year\ 4/Modules/Semester\ 2/Systems\ Software/Assignments/Live/LiveFile.c");
 
  
    
     // Open one file for reading
-    fptr1 = fopen(filename1, "r");
+    fptr1 = fopen(OfflineFile, "r");
     if (fptr1 == NULL)
     {
-        printf("Cannot open file %s \n", filename1);
+        printf("Cannot open file %s \n", OfflineFile);
         exit(0);
     }
  
-    
- 
     // Open another file for writing
-    fptr2 = fopen(filename2, "w");
+    fptr2 = fopen(LiveFile, "w");
     if (fptr2 == NULL)
     {
-        printf("Cannot open file %s \n", filename2);
+        printf("Cannot open file %s \n", LiveFile);
         exit(0);
     }
  
@@ -38,7 +36,7 @@ int main()
         c = fgetc(fptr1);
     }
  
-    printf("\nContents copied to %s", filename2);
+    printf("\nContents copied to %s", LiveFile);
  
     fclose(fptr1);
     fclose(fptr2);
